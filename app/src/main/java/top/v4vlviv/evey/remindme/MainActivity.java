86d63c9f123399a33d38bcad9a651f3d68907123
Evey <byoro777@gmail.com>
2017-07-27 18:36:22 +0300

    Create MAinActivity

---
+++ app/src/main/java/top/v4vlviv/evey/remindme/MainActivity.java
@@ -0,0 +1,17 @@
+package top.v4vlviv.evey.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Evey on 27.07.2017.
+ */
+
+public class MainActivity extends Activity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
