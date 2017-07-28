e7abf337c76bfb9dfe1bfe32dd77641779be81bc
Evey <byoro777@gmail.com>
2017-07-28 03:28:43 +0300

    Navigation menu Tabs

---
+++ app/src/main/java/top/v4vlviv/evey/remindme/Constants.java
@@ -0,0 +1,11 @@
+package top.v4vlviv.evey.remindme;
+
+/**
+ * Created by Evey on 28.07.2017.
+ */
+
+public class Constants {
+    public static final int TAB_ONE = 0;
+    public static final int TAB_TWO = 1;
+    public static final int TAB_THREE = 2;
+}
